3f7e141f068ebe0c2435c556c62fa529130874aa
Andrew Jones <ascjones@gmail.com>
2011-11-04 19:33:32 +0200

    test that we can load the pomodoro back from mongo

--- spec/mongoSpike.js
+++ spec/mongoSpike.js
@@ -19,11 +19,20 @@ var Pomodoro = mongoose.model('Pomodoro');
 vows.describe('Spiking Mongoose').addBatch({
   'Saving a Pomodoro': {
     topic: function() {
-      pomodoro = new Pomodoro({description: 'Test Pomo', started: new Date()});
+      var pomodoro = new Pomodoro({description: 'Test Pomo', started: new Date()});
       pomodoro.save(this.callback);
     },
     'No error should occur': function(err, pomo) {
       assert.isNull(err);
+    },
+    'Loading the pomodoro': {
+      topic: function(pomodoro){
+        Pomodoro.findById(pomodoro.id, this.callback);
+      },
+      'Pomodoro should have been saved': function(err, pomo) {
+        assert.isNotNull(pomo);
+        assert.equal('Test Pomo', pomo.description);
+      }
     }
   }
 }).export(module);
